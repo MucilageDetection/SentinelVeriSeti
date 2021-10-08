c9941edb456e2a9d2de4d23fcfa542422c9bafb8
Bahri ABACI <abacibahri@gmail.com>
2021-10-08 20:02:59 +0000

    New function added to convert multiple jp2 into single mat file

---
+++ saveIMGasMAT.py
@@ -0,0 +1,51 @@
+# Bahri ABACI
+# Save extracted IMG_DATA folder as Matlab mat file
+import matplotlib.image as mpimg
+from scipy.io import savemat
+import os
+
+# folder to unzip
+Directory = 'E:/Dropbox/Dataset/satellite/sentinel2/'
+SentinelGrids = ['35TPF', '35TPE']
+
+# go over the all grids
+for gridName in SentinelGrids:
+    
+    # set the input and output directory
+    InputDirectory = os.path.join(Directory, gridName + '_IMGDATA')
+    OutputDirectory = os.path.join(Directory, gridName + '_MATDATA')
+
+    # create output directory
+    if not os.path.exists(OutputDirectory):
+        print('Creating output directory...')
+        os.makedirs(OutputDirectory)
+
+    for files in os.listdir(InputDirectory):
+        
+        # set the output file directory
+        InputFileDirectory = os.path.join(InputDirectory, files)
+        OutputFileDirectory = os.path.join(OutputDirectory, files)
+        
+        # create output directory
+        if not os.path.exists(OutputFileDirectory):
+            print('Creating output file directory...')
+            os.makedirs(OutputFileDirectory)
+        
+        # go over all resolutions
+        for resolutions in os.listdir(InputFileDirectory):
+            
+            # create array
+            images = {}
+            print(f'creating {resolutions}.mat for {files}')
+            
+            for jp in os.listdir(os.path.join(InputFileDirectory, resolutions)):
+                if jp.endswith(".jp2"):
+                    images[jp[23:26]] = mpimg.imread(os.path.join(InputFileDirectory, resolutions, jp))
+        
+            # save the image
+            savemat(os.path.join(OutputFileDirectory, resolutions + '.mat'), images, do_compression=True)
+            break
+        break
+    break
+# goodbye
+print('Done')
